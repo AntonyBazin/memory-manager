073571887b516fe9895ea86826b1d1e1b02514ef
Anton <antonyvbazin@gmail.com>
2019-10-30 00:06:50 +0300

    project started

---
+++ manager.cpp
@@ -0,0 +1,9 @@
+//
+// Created by antony on 10/29/19.
+//
+
+#include "manager.h"
+
+namespace manager{
+
+}
---
+++ manager.h
@@ -0,0 +1,17 @@
+//
+// Created by antony on 10/29/19.
+//
+
+#ifndef MEMORY_MANAGER_MANAGER_H
+#define MEMORY_MANAGER_MANAGER_H
+
+namespace manager{
+
+    enum requests{ALLOC, FREE};
+
+
+
+}
+
+
+#endif //MEMORY_MANAGER_MANAGER_H
