d491bb03c42d2980782f8b0e62ca2bd8c897bd1e
Anton <antonyvbazin@gmail.com>
2019-11-05 21:11:31 +0300

    the beginning of the h-file

--- manager.cpp
+++ manager.cpp
@@ -2,8 +2,21 @@
 // Created by antony on 10/29/19.
 //
 
+#include <stdatomic.h>
 #include "manager.h"
 
 namespace manager{
 
+    Table &Table::cmd_Table() {
+        static Table instance;
+        return instance;
+    }
+
+    Table::Table() {
+        current_size = 0;
+        used_units_count = 0;
+        programs = nullptr;
+        programs_amount = 0;
+        memory = new int[max_size];
+    }
 }
--- manager.h
+++ manager.h
@@ -6,10 +6,28 @@
 #define MEMORY_MANAGER_MANAGER_H
 
 namespace manager{
+    class Table;
+    class Program;
+    class Entity;
+    class Array;
+    class Link;
+    class Value;
+    class DivSeg;
 
-    enum requests{ALLOC, FREE};
+    class Table{
+    private:
+        static const int max_size = 200;
+        unsigned int current_size;
+        unsigned int used_units_count;
+        Program* programs;
+        unsigned int programs_amount;
+        int *memory;
+        Table();
+    public:
+        Table& cmd_Table();
+    };
 
-
+    class
 
 }
 
