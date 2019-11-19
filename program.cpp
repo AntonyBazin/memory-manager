f6ae531a298e07efc425dd8e372b033bc942d762
Anton <antonyvbazin@gmail.com>
2019-11-19 23:00:26 +0300

    added files with program and table implementations

---
+++ program.cpp
@@ -0,0 +1,50 @@
+//
+// Created by antony on 11/19/19.
+//
+
+#include "manager.h"
+
+
+namespace manager{
+
+    std::string Program::menu[] = {"1. Request memory",
+                                 "2. Free memory",
+                                 "3. Use div segments",
+                                 "4. Show all memory info",
+                                 "5. Show div segs",
+                                 "6. Calculate total memory used"};
+
+    int Program::run(std::iostream& strm) {
+        int rc;
+        while(rc = answer(strm, menu, menus)){
+            (this->*fptr[rc])();
+        }
+        std::cout << "That's all. Bye!" << std::endl;
+        return 0;
+    }
+
+    Program::Program() : memory_quota(50) {
+        entities = {};
+        file_address = "default";
+        fptr[0] = nullptr;
+        fptr[1] = &Program::d_request_memory;
+        fptr[2] = &Program::d_free_memory;
+        fptr[3] = &Program::d_use_divsegs;
+        fptr[4] = &Program::d_show_all;
+        fptr[5] = &Program::d_show_divsegs;
+        fptr[6] = &Program::d_calc_memory;
+    }
+
+    Program::Program(unsigned int t_mem, std::string t_addr) : memory_quota(t_mem) {
+        file_address = std::move(t_addr);
+        entities = {};
+        fptr[0] = nullptr;
+        fptr[1] = &Program::d_request_memory;
+        fptr[2] = &Program::d_free_memory;
+        fptr[3] = &Program::d_use_divsegs;
+        fptr[4] = &Program::d_show_all;
+        fptr[5] = &Program::d_show_divsegs;
+        fptr[6] = &Program::d_calc_memory;
+    }
+
+}
---
+++ table.cpp
@@ -0,0 +1,79 @@
+//
+// Created by antony on 11/19/19.
+//
+
+#include "manager.h"
+
+
+namespace manager{
+
+
+    Table::Table() {
+        current_size = 0;
+        memory.insert(memory.begin(), 300, '\0');
+        free_blocks = {};
+        free_blocks.emplace_back(0, max_size, false);
+    }
+
+    void Table::defragmentation() {
+        std::vector<Unit>::iterator vec_it;  // a cycle is used for full defragmentation
+        for(vec_it = free_blocks.begin() + 1; vec_it != free_blocks.end(); ++vec_it){
+            if(vec_it->starter_address == (vec_it - 1)->starter_address + (vec_it - 1)->size){
+                --vec_it;
+                unsigned int sz = (vec_it + 1)->size;
+                free_blocks.erase(vec_it + 1);
+                vec_it->size += sz;
+            }
+        }
+    }
+
+    void Table::mark_free(unsigned int t_strt, unsigned int t_size) noexcept(false) {
+        if(t_strt < 0)
+            throw std::out_of_range("starter address below zero");
+        if(t_strt > max_size)
+            throw std::out_of_range("starter address higher than max size");
+        auto vec_it = free_blocks.begin();
+        for(; vec_it != free_blocks.end(); ++vec_it){
+            if(vec_it->starter_address <= t_strt + t_size &&
+               vec_it->starter_address + vec_it->size >= t_size + t_strt)
+                throw std::invalid_argument("attempt to free not allocated memory - r-end");
+            if(vec_it->starter_address <= t_strt &&
+               vec_it->starter_address + vec_it->size > t_strt)
+                throw std::invalid_argument("attempt to free not allocated memory - l-end");
+        }
+
+        auto mark = std::find_if(free_blocks.begin(),
+                                 free_blocks.end(),
+                                 [t_strt](Unit un) -> bool { return un.starter_address > t_strt; });
+        Unit newcomer(t_strt, t_size, false);
+        free_blocks.insert(mark, newcomer);
+    }
+
+    Entity* Table::allocate_memory(unsigned int t_size, Entity_ID id) noexcept(false) {
+        auto mark = std::find_if(free_blocks.begin(),
+                                 free_blocks.end(),
+                                 [t_size](Unit un) -> bool { return un.size >= t_size; });
+
+        if(mark == free_blocks.end()){
+            defragmentation();
+            mark = std::find_if(free_blocks.begin(),
+                                free_blocks.end(),
+                                [t_size](Unit un) -> bool { return un.size > t_size; });
+            if(mark == free_blocks.end()) throw std::runtime_error("not enough memory");
+        }
+
+        unsigned int strt = mark->starter_address;
+
+        if(mark->size == t_size){
+            free_blocks.erase(mark);
+        } else{
+            mark->starter_address += t_size;
+            mark->size -= t_size;
+        }
+
+        Entity* new_entity = Entity::create_Entity(id);
+        Unit pos(strt, t_size, true);
+        new_entity->set_pos(pos);
+        return new_entity;
+    }
+}
